013c7032ffbfd1c36a847962b54ca30e94bb4e93
Minseok Jung <37059223+Minseok0917@users.noreply.github.com>
2023-10-08 15:45:52 +0900

    fix : GG

--- js/common.js
+++ js/common.js
@@ -2,7 +2,7 @@ const createElement = (element, attrs = {}) => Object.assign(document.createElem
 
 const state = {
   slideIndex: 0,
-  slideImages: ["/images/slide01.jpg", "/images/slide02.jpg", "/images/slide03.png"],
+  slideImages: ["/images/slide01.png", "/images/slide02.jpg", "/images/slide03.jpg"],
   canvasImages: [],
   get currentSlideImage() {
     return this.canvasImages[this.slideIndex];
@@ -37,7 +37,30 @@ async function getImage(imagePath) {
   });
 }
 async function setCanvasImages() {
-  state.canvasImages = await Promise.all(state.slideImages.map((imagePath) => getImage(imagePath)));
+  const $backgroundCanvas = createElement("canvas", {
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+  const backgroundContext = $backgroundCanvas.getContext("2d");
+  const slideImages = await Promise.all(state.slideImages.map((imagePath) => getImage(imagePath)));
+  state.canvasImages = slideImages.map((slideImage) => {
+    const { $image, width, height } = slideImage;
+    backgroundContext.drawImage($image, 0, 0, width, height);
+    slideImage.datas = backgroundContext.getImageData(0, 0, $canvas.width, $canvas.height).data;
+    slideImage.colors = slideImage.datas.reduce(
+      (acc, rgba, index) => {
+        acc.color.push(rgba);
+        if ((index + 1) % 4 === 0) {
+          acc.colors.push(acc.color);
+          acc.color = [];
+        }
+
+        return acc;
+      },
+      { colors: [], color: [] }
+    ).colors;
+    return slideImage;
+  });
 }
 
 function getRatio(width, height) {
@@ -52,17 +75,35 @@ function getRatio(width, height) {
 
 function renderSlide() {
   const slideImage = state.currentSlideImage;
-  const { $image, width, height } = slideImage;
-  context.drawImage($image, 0, 0, width, height);
+  const canvasWidth = $canvas.width;
+  const canvasHeight = $canvas.height;
+  const limit = 500;
+  const limitPlus = 10;
+  slideImage.colors.slice(canvasWidth * limit, canvasWidth * (limit + limitPlus)).forEach(([r, g, b], index) => {
+    const x = index % canvasWidth;
+    const y = Math.floor(index / canvasWidth);
+
+    context.fillStyle = `rgba(${r},${g},${b},255)`;
+
+    context.rect(x, y + limit, 1, 1);
+    context.fill();
+  });
 }
 
 async function init() {
+  console.time("settings");
   await setCanvasImages();
-  setInterval(function () {
-    if (++state.slideIndex === state.slideImages.length) state.slideIndex = 0;
-    renderSlide();
-  }, 3000);
+  console.timeEnd("settings");
+  /* setInterval(function () {
+      if (++state.slideIndex === state.slideImages.length) state.slideIndex = 0;
+      renderSlide();
+    }, 3000); */
+  //   console.time("render");
+  //   console.log(state.canvasImages);
+  console.time("render");
   renderSlide();
+  console.timeEnd("render");
+  //   console.timeEnd("render");
 }
 init();
 document.body.append($canvas);
